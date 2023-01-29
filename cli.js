5034b9c0c42c4616321025f44d90bff1f870fbe8
pjgregso <pjgregso@email.unc.edu>
2023-01-28 19:27:56 -0500

    workaround 4, weirdness...

--- cli.js
+++ cli.js
@@ -5,11 +5,13 @@ import moment from "moment-timezone";
 import minimist from "minimist";
 import fetch from "node-fetch";
 
+// Use minimist process command line arguments
 const args = minimist(process.argv.slice(2));
 
-//Handle help command
+// Handle help command
 if("h" in args) {
     console.log(
+
 `
 Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
 -h            Show this help message and exit.
@@ -19,50 +21,60 @@ Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
 -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
 -j            Echo pretty JSON from open-meteo API and exit.
 `
-);
+    );
     process.exit(0);
 }
 
-//handle latitude and longitude for API
 let latitude = undefined;
 let longitude = undefined;
-let timezone = moment.tz.guess();
 
-
-if ("n" in args){
+if("n" in args) {
     latitude = args["n"];
-} else if ("s" in args){
+}else if("s" in args) {
     latitude = -args["s"];
 }
 
-if ("e" in args){
-    longitude = ["e"];
-} else if ("w" in args){
+if("e" in args) {
+    longitude = args["e"];
+}else if("w" in args) {
     longitude = -args["w"];
 }
 
+// Check to ensure that latitude and longitude are in the valid range
+if(latitude === undefined || Math.abs(latitude) > 90) {
+    console.log("Latitude must be in range");
+    process.exit(0);
+}
+if(longitude === undefined || Math.abs(longitude) > 180) {
+    console.log("Longitude must be in range");
+    process.exit(0);
+}
+let timezone = moment.tz.guess();
 
-
-if ("t" in args){
-    timezone = args["t"];
+if("t" in args) {
+    timezone = args["t"];    
 }
 
-const request_url = 'https://api.open-meteo.com/v1/forecast?latitude='+latitude+'&longitude='+longitude+'&daily=precipitation_hours&current_weather=true&timezone=' + timezone;
+
+
+// Make request to the API
+const request_url = "https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude + "&daily=precipitation_hours&current_weather=true&timezone=" + timezone;
 const response = await fetch(request_url);
 
+// Get the data from the request
 const data = await response.json();
 
-// If JSON flags print all JSON, exit
 if("j" in args) {
     console.log(data);
     process.exit(0);
 }
+
 const days = args["d"];
-//Days
+
 if (days == 0) {
-    console.log("At coordinates: (" + latitude + ", " + longitude + "), it will rain " + data["daily"]["precipitation_hours"][0] + " hours today.`\n");
+    console.log("At location (" + latitude + ", " + longitude + "), it will rain " + data["daily"]["precipitation_hours"][0] + " hours today.\n");
 } else if (days > 1) {
-    console.log("At coordinates: (" + latitude + ", " + longitude + "), it will rain " + data["daily"]["precipitation_hours"][0] + " hours in " + days + " days.\n");
+    console.log("At location (" + latitude + ", " + longitude + "), it will rain " + data["daily"]["precipitation_hours"][0] + " hours in " + days + " days.\n");
 } else {
-    console.log("At coordinates: (" + latitude + ", " + longitude + "), it will rain " + data["daily"]["precipitation_hours"][0] + " hours tomorrow.\n");
+    console.log("At location (" + latitude + ", " + longitude + "), it will rain " + data["daily"]["precipitation_hours"][0] + " hours tomorrow.\n");
 }
