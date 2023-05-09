ebb09ed4878a252b11216c0cda1b8ce2098ad179
pjgregso <pjgregso@email.unc.edu>
2023-05-09 18:59:06 -0400

    futurebirds

--- cli.js
+++ cli.js
@@ -13,7 +13,7 @@ if("h" in args) {
 `
 Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
 -h            Show this help message and exit.
--n, -s        Latitude: N positive; S negative.
+-n, -s        Latitude: N positive; S negative. 
 -e, -w        Longitude: E positive; W negative. 
 -z            Time zone: uses tz.guess() from moment-timezone by default.
 -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
