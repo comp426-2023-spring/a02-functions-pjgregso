1a6615321a6144ca6f67307bb335e2c3c21427e6
pjgregso <pjgregso@email.unc.edu>
2023-05-09 18:59:54 -0400

    okay

--- cli.js
+++ cli.js
@@ -17,7 +17,7 @@ Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
 -e, -w        Longitude: E positive; W negative.  
 -z            Time zone: uses tz.guess() from moment-timezone by default.
 -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
--j            Echo pretty JSON from open-meteo API and exit.
+-j            Echo pretty JSON from open-meteo API and exit. 
 `
     );
     process.exit(0);
